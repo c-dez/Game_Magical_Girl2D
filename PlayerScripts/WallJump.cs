38e739d3ad5675878d1611683363e2dfcacf68fd
c-dez <hdezcruzcarlos@gmail.com>
2024-03-16 17:01:06 -0600

    Added Gizmo.DrawWiredCube() in player to detect walls

--- PlayerScripts/WallJump.cs
+++ PlayerScripts/WallJump.cs
@@ -1,15 +1,25 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class WallJump : MonoBehaviour
 {
-    
-    void Start()
+    [Header("Boxcast")]
+    [SerializeField] private BoxCollider2D boxColider;
+    [SerializeField] private Vector3 boxSize;
+    private void OnDrawGizmos()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(boxColider.bounds.center, boxSize);
     }
 }
+/*
+walljump
+con un boxcast detectar muros con tag
+checar la direccion del input, hacia muro entra en
+slide state
+cambiar friccion y velocidad de caida
+poder brincar
+
+
+
+*/
