1dc501ce10e2e1fd6b484c2823806618c035d13c
c-dez <hdezcruzcarlos@gmail.com>
2024-03-16 17:31:56 -0600

    Added RaycastHit2D to HittingWall() returns true when hitting wall

--- PlayerScripts/WallJump.cs
+++ PlayerScripts/WallJump.cs
@@ -7,17 +7,27 @@ public class WallJump : MonoBehaviour
     [SerializeField] private BoxCollider2D boxColider;
     [SerializeField] private Vector3 boxSize;
     [SerializeField] private LayerMask wallLayer;
+    private bool hittingWall;
 
     private void FixedUpdate()
     {
+        hittingWall = HittingWall();
+    }
 
+    private bool HittingWall()
+    {
         RaycastHit2D hit = 
         Physics2D.BoxCast(boxColider.bounds.center,boxSize,0,Vector2.right,0, wallLayer);
 
         if(hit.collider != null)
         {
-            Debug.Log("hit wall");
+            // Debug.Log("hit wall");
+            return true;
         }
+        else{
+            return false;
+        }
+
     }
     private void OnDrawGizmos()
     {
